3c73abc531131f1ef10233ca082851411d58389e
gghjk <gghjk@DESKTOP-8646QEE.dhcp.wustl.edu>
2025-02-19 14:17:20 -0600

    heres the thing

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,24 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		double x = .4;
+		double y = .7;
+		double x2 = .7;
+		double halfWidth = .05;
+		double halfHeight = .05;
+		
+		double x3 = .555;
+		double y2 = .3;
+		double halfWidth2 = .25;
+		
+		int redcomp = 5;
+		int greencomp = 67;
+		int bluecomp = 89;
+		StdDraw.setPenColor(redcomp, greencomp, bluecomp);
+		
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+		StdDraw.filledRectangle(x2, y, halfWidth, halfHeight);
+		StdDraw.filledRectangle(x3, y2, halfWidth2, halfHeight);
+		
 	}
 }
